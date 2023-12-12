b75f0710c3fa48427e9e62ab34101bfe0ef6e834
Nguyen181124 <nguyen181124@gmail.com>
2023-12-13 06:04:23 +0700

    Hoan thien

--- README.md
+++ README.md
@@ -2,8 +2,22 @@
 Thực hiện bởi: Nguyễn Tuấn Nguyên
 ## Trong bài bao gồm:
 1. Nhập vào 2 số
-1. Kiểm tra 
+1. Kiểm tra
+
+    - ![](img/test_number.png)
+
 1. Tính tổng và in ra kết quả
+
+    - ![](img/add_number.png)
+
 1. Tính hiệu và in ra kết quả
+
+    - ![](img/subtract_number.png)
+
 1. Tính tích và in ra kết quả
+
+    - ![](img/multiply_number.png)
+
 1. Tính thương và in ra kết quả
+
+    - ![](img/divide_number.png)
--- index.php
+++ index.php
@@ -1,43 +1,88 @@
 <?php
-    $number1 = $_POST['number1'];
-    $number2 = $_POST['number2'];
-if(isset($_POST['Add'])){
-  $result = $number1 + $number2;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["number1"])) {
+        $enumber1 = "Hãy nhập số thứ nhất";
+    } elseif (!is_numeric($_POST["number1"])) {
+        $enumber1 = "Số thứ nhất không hợp lệ";
+    } else {
+        $number1 = $_POST['number1'];
+    }
+
+    if (empty($_POST["number2"])) {
+        $enumber2 = "Hãy nhập số thứ hai";
+    } elseif (!is_numeric($_POST["number2"])) {
+        $enumber2 = "Số thứ hai không hợp lệ";
+    } else {
+        $number2 = $_POST['number2'];
+    }
 }
 
-if(isset($_POST['Subtract'])){
-    $result = $number1 - $number2;
+
+if (isset($_POST['Add'])) {
+    if ($enumber1 == "" && $enumber2 == "") {
+        $result = $number1 + $number2;
+    }
 }
 
-if(isset($_POST['Multiply'])){
-    $result = $number1 * $number2;
+if (isset($_POST['Subtract'])) {
+    if ($enumber1 == "" && $enumber2 == "") {
+        $result = $number1 - $number2;
+    }
+}
+
+if (isset($_POST['Multiply'])) {
+    if ($enumber1 == "" && $enumber2 == "") {
+        $result = $number1 * $number2;
+    }
 }
 
 if (isset($_POST['Divide'])) {
-    $result = $number1 / $number2;
+    if ($enumber1 == "" && $enumber2 == "") {
+        if ($number2 == 0) {
+            $result = "Không thể chia cho 0";
+        } else {
+            $result = $number1 / $number2;
+        }
+    }
 }
 ?>
-<!DOCTYPE HTML>  
+<!DOCTYPE HTML>
 <html>
-<head>
-<style>
-    input {
-        margin-bottom: 10px;
-    }
-</style>
-</head>
-<body>  
-<h2>PHP - Simple Calculator Program</h2>
-<form action="" method="post">
-  <input type="number" name="number1" value='<?php echo $number1;?>'/> First second<br>
 
-  <input type="number" name="number2" value='<?php echo $number2;?>'/> Second Number<br>
-  <input readonly type="text" name="result" value='<?php echo $result;?>'/> Result<br>
-  <input type="submit" name="Add" value="Add" />
-  <input type="submit" name="Subtract" value="Subtract" />
-  <input type="submit" name="Multiply" value="Multiply" />
-  <input type="submit" name="Divide" value="Divide" />
-</form>
+<head>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        input {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <h2>PHP - Simple Calculator Program</h2>
+    <form action="" method="post">
+        <input type="text" name="number1" value='<?php echo $number1; ?>' /> First second<br>
+        <span class="error">
+            <?php echo $enumber1; ?>
+        </span>
+        <br>
+        <input type="text" name="number2" value='<?php echo $number2; ?>' /> Second Number<br>
+        <span class="error">
+            <?php echo $enumber2; ?>
+        </span>
+        <br>
+        <input readonly type="text" name="result" value='<?php echo $result; ?>' /> Result<br>
+        <input type="submit" name="Add" value="Add" />
+        <input type="submit" name="Subtract" value="Subtract" />
+        <input type="submit" name="Multiply" value="Multiply" />
+        <input type="submit" name="Divide" value="Divide" />
+    </form>
 
 </body>
+
 </html
